8003b26262f50acb945880a1865cca7364705381
Hamma Azarok <hammaagazarok@gmail.com>
2023-02-18 11:59:33 +0200

    Add Prepend Function

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,14 +1,30 @@
-
+type Node<T> = {
+    value: T,
+    prev?: Node<T>,
+    next?: Node<T>,
+}
 export default class DoublyLinkedList<T> {
     public length: number;
+    public head?: Node<T>;
 
     
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     prepend(item: T): void {
+    const node = {value: item} as Node<T>;
+    this.length ++;
+    if(!this.head){
+        this.head = node;
+        return;
+    }
 
+    node.next = this.head;
+    this.head.prev = node;
+    this.head = node;
 }
     insertAt(item: T, idx: number): void {
 
