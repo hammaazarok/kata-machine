54b902b2e4eb20f5860b73eda7b9b140fc87d4ef
Hamma Azarok <hammaagazarok@gmail.com>
2023-02-18 12:19:19 +0200

    Add Insert At Function

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,44 +1,61 @@
+import { createSourceFile } from "typescript";
+
 type Node<T> = {
-    value: T,
-    prev?: Node<T>,
-    next?: Node<T>,
-}
+    value: T;
+    prev?: Node<T>;
+    next?: Node<T>;
+};
 export default class DoublyLinkedList<T> {
     public length: number;
     public head?: Node<T>;
-
-    
+    public tail?: Node<T>;
 
     constructor() {
         this.length = 0;
         this.head = undefined;
+        this.tail = undefined;
     }
 
     prepend(item: T): void {
-    const node = {value: item} as Node<T>;
-    this.length ++;
-    if(!this.head){
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        node.next = this.head;
+        this.head.prev = node;
         this.head = node;
-        return;
     }
-
-    node.next = this.head;
-    this.head.prev = node;
-    this.head = node;
-}
     insertAt(item: T, idx: number): void {
-
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+        if (idx > this.length) {
+            throw new Error("Index is out of List's Length");
+        }
+        if (idx === this.length) {
+            this.append(item);
+            return;
+        }
+        if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
+        this.length++;
+        let curr = this.head;
+        for (let i = 0; curr && i < idx; i++) {
+            curr = curr.next;
+        }
+        curr = curr as Node<T>;
+        const node = { value: item } as Node<T>;
+        node.next = curr;
+        node.prev = curr.prev;
+        curr.prev = node;
+        if (curr.prev) {
+            curr.prev.next = curr;
+        }
+    }
+    append(item: T): void {}
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}
 }
